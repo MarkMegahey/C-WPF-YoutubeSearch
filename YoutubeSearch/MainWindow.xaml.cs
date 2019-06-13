ecbadd6d6ac5bf168e37a0d20570ad5ae6e4c7da
SDC-GROUP\mark <mark@smartdriverclub.co.uk>
2019-06-13 11:26:40 +0100

    Add project files.

---
+++ YoutubeSearch/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+
+using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+
+using YoutubeSearch.Models;
+
+
+namespace YoutubeSearch
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<YTVideo> videos;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void YoutubeSearchButton_Button_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            await LoadDataCollection();
+            YoutubeSearchDisplay.ItemsSource = videos;
+        }
+
+        private async Task LoadDataCollection()
+        {
+            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = "AIzaSyB1OOSpTREs85WUMvIgJvLTZKye4BVsoFU",
+                ApplicationName = this.GetType().ToString()
+            });
+
+            var searchListRequest = youtubeService.Search.List("snippet");
+            searchListRequest.Q = YoutubeSearchBar_Textbox.Text;
+            searchListRequest.MaxResults = 50;
+
+            // Call the search.list method to retrieve results matching the specified query term.
+            var searchListResponse = await searchListRequest.ExecuteAsync();
+
+            videos = new List<YTVideo>();
+
+            foreach (var searchResult in searchListResponse.Items)
+            {
+                switch (searchResult.Id.Kind)
+                {
+                    case "youtube#video":
+                        YTVideo video = new YTVideo();
+                        video.Title = searchResult.Snippet.Title;
+                        video.Author = searchResult.Snippet.ChannelTitle;
+                        video.URL = $"https://www.youtube.com/watch?v={searchResult.Id.VideoId}";
+                        byte[] imageBytes = new WebClient().DownloadData(searchResult.Snippet.Thumbnails.Default__.Url);
+                        using (MemoryStream ms = new MemoryStream(imageBytes))
+                        {
+                            var imageSource = new BitmapImage();
+                            imageSource.BeginInit();
+                            imageSource.StreamSource = ms;
+                            imageSource.EndInit();
+
+                            // Assign the Source property of your image
+                            video.Thumbnail = new Image { Source = imageSource };
+                        }
+                        videos.Add(video);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ YoutubeSearch/Models/YTVideo.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace YoutubeSearch.Models
+{
+    public class YTVideo
+    {
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string URL { get; set; }
+
+        public Image Thumbnail { get; set; }
+    }
+}
